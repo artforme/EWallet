41b83a566ed712658696af9a13f5ffc4abab37a3
artforme <me.peremy@gmail.com>
2024-01-23 04:24:51 +0300

    feat: add validator

--- internal/lib/api/response/response.go
+++ internal/lib/api/response/response.go
@@ -1,5 +1,11 @@
 package response
 
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"strings"
+)
+
 type RespTransaction struct {
 	TransactionTime string `json:"transactionTime"`
 	FromWallet      string `json:"fromWallet"`
@@ -8,19 +14,19 @@ type RespTransaction struct {
 }
 
 type Wallet struct {
-	WalletID string `json:"wallet_id"`
-	Balance  string `json:"balance"`
+	WalletID string `json:"wallet_id,omitempty"`
+	Balance  string `json:"balance,omitempty"`
 }
 
 type Response struct {
 	Status string `json:"status"`
 	Error  string `json:"error,omitempty"`
-	Wallet
 }
 
 const (
-	StatusOK    = "200"
-	StatusError = "Error"
+	StatusOK            = "200"
+	StatusError         = "400"
+	StatusErrorNotFound = "404"
 )
 
 func OK() Response {
@@ -33,5 +39,32 @@ func ResError(msg string) Response {
 	return Response{
 		Status: StatusError,
 		Error:  msg,
+	}
+}
+
+func ResErrorNotFound(msg string) Response {
+	return Response{
+		Status: StatusErrorNotFound,
+		Error:  msg,
+	}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is a required field", err.Field()))
+		case "url":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not a valid URL", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
+		}
+	}
+
+	return Response{
+		Status: StatusError,
+		Error:  strings.Join(errMsgs, ", "),
 	}
 }
