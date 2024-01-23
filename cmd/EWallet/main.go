f6393abf95b3db75b5ab899471403952a2831c42
artforme <me.peremy@gmail.com>
2024-01-23 21:24:51 +0300

    chore: add new comments

--- cmd/EWallet/main.go
+++ cmd/EWallet/main.go
@@ -36,12 +36,16 @@ func main() {
 	}
 
 	router := chi.NewRouter()
-
+	// using middleware RequestID to know ID client in log
 	router.Use(middleware.RequestID)
+	// using our customized log
 	router.Use(mwLogger.New(log))
+	// this middleware recover process if some handler caught a mistake
 	router.Use(middleware.Recoverer)
+	// this middleware allows us read some info form url
 	router.Use(middleware.URLFormat)
 
+	// There are some handlers that we use in specific method and url
 	router.Post("/api/v1/wallet", createWallet.New(log, storage))
 	router.Post("/api/v1/wallet/{walletId}/send", transfer.New(log, storage))
 	router.Get("/api/v1/wallet/{walletId}/history", showHistory.New(log, storage))
@@ -49,6 +53,7 @@ func main() {
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
+	// setup server
 	srv := &http.Server{
 		Addr:         cfg.Address,
 		Handler:      router,
